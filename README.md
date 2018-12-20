fe4e1e22567073f390ba5c45c1f86b91045bb307
李永龙 <liyonglong@xiaomi.com>
2018-12-20 16:02:40 +0800

    optimize

--- README.md
+++ README.md
@@ -1,85 +1,81 @@
-## A H5 video component for vue 
+## A H5 video component for vue
 
 ### feature
 
-> Auto size when playing;               
-> Suport custom init width and height;                       
-> Suport speed and retreat when playing;          
-> Suport full screen when playing           
+> Auto size when playing;  
+> Suport custom init width and height;  
+> Suport speed and retreat when playing;  
+> Suport full screen when playing
 
 ### install
+
 ```javascript
  npm i vue-h5-video --save
 ```
-### import methods
 
+### import methods
 
 #### global import
 
 ```javascript
-import { H5Video } from 'vue-h5-video'
-Vue.use(H5Video)
+import { H5Video } from 'vue-h5-video';
+Vue.use(H5Video);
 ```
 
 #### import in some component
 
 ```javascript
-import { H5Video } from 'vue-h5-video'
+import { H5Video } from 'vue-h5-video';
 
 export default {
   components: {
-     H5Video
-  }
-}
-
+    H5Video,
+  },
+};
 ```
+
 ### usage
 
 ```html
-<h5-video 
-   @play="play"
-   initHeight="480px"
-   :sources="sources"  
-   poster="http://img.v.trend.mi.rozbuzz.com/wemedia/cover/89c3cf50ae0911e8988e593b4fd90442/61d6562ed1a7c506d50a8e7c67f949fb.png-290.webp"
-   src="http://intl.w.xk.miui.com/451a4b2ff292392e9eb85907904645ce"
-   >
+<h5-video
+  @play="play"
+  initHeight="480px"
+  :sources="sources"
+  poster="http://img.v.trend.mi.rozbuzz.com/wemedia/cover/89c3cf50ae0911e8988e593b4fd90442/61d6562ed1a7c506d50a8e7c67f949fb.png-290.webp"
+  src="http://intl.w.xk.miui.com/451a4b2ff292392e9eb85907904645ce"
+>
 </h5-video>
-
 ```
 
 #### props：
 
-
-|prop|type|default|explain|
-|-|-|-|-|
-|src|String|undefined|video's source link|
-|poster|String|undefined|video's poster|
-|title|String|''|video's title|
-|titleWidth|String|calc(100% - 105px)|size of title|
-|showControlBar|Boolean|true|allow control|
-|sources|Array|[]|video's source link|
-|autoplay|Boolean|false|autoplay when video is loaded|
-|loop|Boolean|false|replay when playing is end
-|preload|String|'auto'|load method ,must be one of 'auto','mete' or 'none'|
-|initWidth|String|'100%'|init video's width|
-|initHeight|String|'auto'|init video's height|
-|iconWidth|String|'4%'|size of play and pause icon|
-|playIcon|String|default path|custom playIcon|
-|pauseIcon|String|default path|custom pauseIcon|
-|hasWatermark|Boolean|false|support watermark|
-|watermark|String|undefined|watermark url|
-|watermarkWidth|Boolean|60px|size of watermark|
-|errMsg|String|'加载失败，请检查网络！'|error tip|
- 
+| prop           | type    | default                  | explain                                             |
+| -------------- | ------- | ------------------------ | --------------------------------------------------- |
+| src            | String  | undefined                | video's source link                                 |
+| poster         | String  | undefined                | video's poster                                      |
+| title          | String  | ''                       | video's title                                       |
+| showControlBar | Boolean | true                     | allow control                                       |
+| sources        | Array   | []                       | video's source link                                 |
+| autoplay       | Boolean | false                    | autoplay when video is loaded                       |
+| loop           | Boolean | false                    | replay when playing is end                          |
+| preload        | String  | 'auto'                   | load method ,must be one of 'auto','mete' or 'none' |
+| initWidth      | String  | '100%'                   | init video's width                                  |
+| initHeight     | String  | 'auto'                   | init video's height                                 |
+| iconWidth      | String  | '4%'                     | size of play and pause icon                         |
+| playIcon       | String  | default path             | custom playIcon                                     |
+| pauseIcon      | String  | default path             | custom pauseIcon                                    |
+| hasWatermark   | Boolean | false                    | support watermark                                   |
+| watermark      | String  | undefined                | watermark url                                       |
+| watermarkWidth | Boolean | 60px                     | size of watermark                                   |
+| errMsg         | String  | '加载失败，请检查网络！' | error tip                                           |
 
 #### events
 
-|event name|
-|-|
-|play|
-|pause|
-|ended|
-|canplay|
-|error|
-|timeupdate|
-
+| event name |
+| ---------- |
+| play       |
+| pause      |
+| ended      |
+| canplay    |
+| error      |
+| timeupdate |
--- src/Video.vue
+++ src/Video.vue
@@ -1,92 +1,91 @@
 <template>
-    <div class="video-wrapper"
-        :style="{'width': width, 'height': height}"
+  <div class="video-wrapper" :style="{'width': width, 'height': height}">
+    <video
+      ref="video"
+      :src="src"
+      :autoplay="autoplay"
+      :loop="loop"
+      :preload="preload"
+      @ended="resetVideo"
+      @click="touch"
+      @canplay.once="preparePlay"
+      @error="errorHandler"
+      @timeupdate="progressHandler"
+    >
+      <source v-for="(item, index) in sources" :key="index" :src="item">
+    </video>
+
+    <div
+      class="poster"
+      :class="{'loading': isLoading}"
+      v-if="!start && !autoplay"
+      :style="{background: 'url(' + poster + ')'}"
+    ></div>
+
+    <div class="topbar">
+      <div>
+        <img
+          v-if="hasWatermark"
+          :src="watermark"
+          :style="{'width': watermarkWidth, 'min-width': '80px', 'max-width': '180px'}"
+          alt="watermark"
+          class="watermark"
         >
-        <video ref='video'
-         :src="src" 
-         :autoplay="autoplay"
-         :loop="loop"
-         :preload="preload"
-         @ended="resetVideo"
-         @click="touch"
-         @canplay.once="preparePlay"
-         @error="errorHandler"
-         @timeupdate="progressHandler"
-        >
-         <source v-for="(item, index) in sources" :key="index" :src="item">
-        </video>
+      </div>
 
-        <div class="poster" :class="{'loading': isLoading}" v-if="!start && !autoplay"
-         :style="{background: 'url(' + poster + ')'}"
-        >
-        </div>
+      <span class="title" v-show="isControl">{{title}}</span>
 
-        <div class="topbar">
-            <img v-if="hasWatermark"
-                :src="watermark" 
-                :style="{'width': watermarkWidth, 'min-width': '80px', 'max-width': '180px'}" 
-                alt="watermark" class="watermark" 
-            >
-            <span class="title" v-show="isControl" :style="{'width': titleWidth, 'display': 'inline-block'}">{{title}}</span>
-
-            <!-- <p class="title" v-show="isControl" :style="{'width': titleWidth}">{{title}}</p> -->
-        </div>
-        
-
-        <div class="error" v-if="isError">
-            {{errMsg}}
-        </div>
-        
-        <div class="control"
-         v-show="isControl"
-         
-        >
-            <img v-show="isPause && !isLoading"
-                @click="play" 
-                :src="playIcon" 
-                :style="{'width': iconWidth, 'min-width': '40px'}" 
-                alt="play button" 
-                class="play-icon"
-             >
-            <img v-show="!isPause"
-                @click="pause" 
-                :src="pauseIcon" 
-                :style="{'width': iconWidth, 'min-width': '40px'}" 
-                alt="pause button" 
-                class="pause-icon"
-            >
-            <div class="progress" 
-               v-show="showControlBar && !isLoading"
-            >
-                <div class="icon">
-                    <button class="play-icon" v-show="isPause" @click="play"></button>
-                    <button class="pause-icon" v-show="!isPause" @click="pause"></button>
-                </div>
-                <div class="progress-bar"
-                  @touchstart="moveStart"
-                  @touchmove="moveVideo"
-                  @touchend="moveEnd"
-                  @mousedown="moveStart"
-                  @mousemove="moveVideo"
-                  @mouseup="moveEnd"
-                >
-                    <div class="has-play" ref="hasPlay"></div>
-                </div>
-
-                <div class="time">
-                    {{current}} / {{duration}}
-                </div>
-
-                <div class="screen-btn" @click="fullScreen">
-                    <button class="full-screen"></button>
-                </div>
-            </div>
-        </div>
+      <!-- <p class="title" v-show="isControl" :style="{'width': titleWidth}">{{title}}</p> -->
     </div>
+
+    <div class="error" v-if="isError">{{errMsg}}</div>
+
+    <div class="control" v-show="isControl">
+      <img
+        v-show="isPause && !isLoading"
+        @click="play"
+        :src="playIcon"
+        :style="{'width': iconWidth, 'min-width': '40px'}"
+        alt="play button"
+        class="play-icon"
+      >
+      <img
+        v-show="!isPause"
+        @click="pause"
+        :src="pauseIcon"
+        :style="{'width': iconWidth, 'min-width': '40px'}"
+        alt="pause button"
+        class="pause-icon"
+      >
+      <div class="progress" v-show="showControlBar && !isLoading">
+        <div class="icon">
+          <button class="play-icon" v-show="isPause" @click="play"></button>
+          <button class="pause-icon" v-show="!isPause" @click="pause"></button>
+        </div>
+        <div
+          class="progress-bar"
+          @touchstart="moveStart"
+          @touchmove="moveVideo"
+          @touchend="moveEnd"
+          @mousedown="moveStart"
+          @mousemove="moveVideo"
+          @mouseup="moveEnd"
+        >
+          <div class="has-play" ref="hasPlay"></div>
+        </div>
+
+        <div class="time">{{current}} / {{duration}}</div>
+
+        <div class="screen-btn" @click="fullScreen">
+          <button class="full-screen"></button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'h5-video',
+  name: "h5-video",
   props: {
     src: String,
     poster: String,
@@ -94,107 +93,105 @@ export default {
     title: {
       type: String,
       default: () => {
-        return '';
-      },
+        return "";
+      }
     },
     titleWidth: {
       type: String,
       default: () => {
-        return 'calc(100% - 105px)';
-      },
+        return "calc(100% - 105px)";
+      }
     },
     showControlBar: {
       type: Boolean,
       default: () => {
         return true;
-      },
+      }
     },
     sources: {
       type: Array,
       default: () => {
         return [];
-      },
+      }
     },
     autoplay: {
       type: Boolean,
       default: () => {
         return false;
-      },
+      }
     },
     loop: {
       type: Boolean,
       default: () => {
         return false;
-      },
+      }
     },
     preload: {
       type: String,
       default: () => {
-        return 'auto';
+        return "auto";
       },
       validator: function(val) {
-        return val === 'auto' || val === 'mete' || val === 'none';
-      },
+        return val === "auto" || val === "mete" || val === "none";
+      }
     },
     initWidth: {
       type: String,
       default: () => {
-        return '100%';
-      },
+        return "100%";
+      }
     },
 
     initHeight: {
       type: String,
       default: () => {
-        return 'auto';
-      },
+        return "auto";
+      }
     },
 
     iconWidth: {
       type: String,
       default: () => {
-        return '4%';
-      },
+        return "4%";
+      }
     },
 
     watermarkWidth: {
       type: String,
       default: () => {
-        return '60px';
-      },
+        return "60px";
+      }
     },
 
     hasWatermark: {
       type: Boolean,
       default: () => {
         return false;
-      },
+      }
     },
 
     playIcon: {
       type: String,
       default: () => {
-        return require('./img/play.png');
-      },
+        return require("./img/play.png");
+      }
     },
 
     pauseIcon: {
       type: String,
       default: () => {
-        return require('./img/pause.png');
-      },
+        return require("./img/pause.png");
+      }
     },
     errMsg: {
       type: String,
       default: () => {
-        return '加载失败，请检查网络！';
-      },
-    },
+        return "加载失败，请检查网络！";
+      }
+    }
   },
   data() {
     return {
-      width: this.initWidth,
-      height: this.initHeight,
       isPause: true,
       isTouch: false,
       isControl: false,
@@ -205,9 +202,17 @@ export default {
       isLoading: true,
       isError: false,
       moveStarted: true,
-      duration: '00:00:00',
-      current: '00:00:00',
+      duration: "00:00:00",
+      current: "00:00:00"
     };
+  },
+  computed: {
+    height: function() {
+      return this.initHeight;
+    },
+    width: function() {
+      return this.initWidth;
+    }
   },
   mounted() {
     this.$nextTick(function() {
@@ -226,7 +231,7 @@ export default {
       if (this.timer) {
         clearTimeout(this.timer);
       }
-      this.$emit('ended');
+      this.$emit("ended");
     },
     touch() {
       if (!this.isControl) {
@@ -240,17 +245,15 @@ export default {
     },
     play() {
       if (this.isPause) {
-        console.log(99);
         (this.start = true), this.video.play();
         this.isPause = false;
         this.isControl = false;
         let realVideo = this.video;
         realVideo.parentNode.style.height =
-          realVideo.videoHeight *
-            realVideo.parentNode.clientWidth /
+          (realVideo.videoHeight * realVideo.parentNode.clientWidth) /
             realVideo.videoWidth +
-          'px';
-        this.$emit('play');
+          "px";
+        this.$emit("play");
       }
     },
     pause() {
@@ -261,14 +264,14 @@ export default {
         this.video.pause();
         this.isPause = true;
         this.isControl = true;
-        this.$emit('pause');
+        this.$emit("pause");
       }
     },
     progressHandler() {
       this.current = this.timeFormatter(this.video.currentTime);
       this.hasPlay.style.width =
-        100 * this.video.currentTime / this.video.duration + '%';
-      this.$emit('timeupdate');
+        (100 * this.video.currentTime) / this.video.duration + "%";
+      this.$emit("timeupdate");
     },
     preparePlay() {
       this.duration = this.timeFormatter(this.video.duration);
@@ -278,17 +281,17 @@ export default {
       if (!this.autoplay) {
         this.isControl = true;
       }
-      this.$emit('canplay');
+      this.$emit("canplay", this.video);
     },
     timeFormatter(input) {
       let time = Math.floor(input),
         h = parseInt(time / 3600),
         m = parseInt((time % 3600) / 60),
         s = parseInt(time % 60);
-      h = h < 10 ? '0' + h : h;
-      m = m < 10 ? '0' + m : m;
-      s = s < 10 ? '0' + s : s;
-      return h + ':' + m + ':' + s;
+      h = h < 10 ? "0" + h : h;
+      m = m < 10 ? "0" + m : m;
+      s = s < 10 ? "0" + s : s;
+      return h + ":" + m + ":" + s;
     },
     fullScreen() {
       this.video.webkitRequestFullScreen();
@@ -301,7 +304,7 @@ export default {
         cur =
           (e.clientX || e.targetTouches[0].clientX) -
           e.currentTarget.offsetLeft;
-      this.video.currentTime = this.video.duration * cur / w;
+      this.video.currentTime = (this.video.duration * cur) / w;
       this.$forceUpdate();
     },
     moveStart() {
@@ -318,12 +321,12 @@ export default {
     },
     errorHandler() {
       setTimeout(() => {
-        this.$emit('error');
+        this.$emit("error");
         this.isLoading = false;
         this.isError = true;
       }, 400);
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped src="./css/index.css"></style>
